bf7264adc7332c6b16618131790664f6dae6ce19
WillMuzyka <wmonblog@gmail.com>
2020-04-26 02:36:28 -0300

    Fix: db config for Heroku Data

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,25 @@
 // configuration for the database
 const { Pool } = require("pg")
 
-module.exports = new Pool({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME
-})
+function generateDB() {
+	if(process.env.NODE_ENV === 'development') {
+		const newDb = new Pool({
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
+			database: process.env.DB_NAME
+		})
+		return newDb
+	} else {
+		const client = new Client({
+			connectionString: process.env.DATABASE_URL,
+			ssl: true,
+		})
+		
+		client.connect();
+		return client
+	}
+}
+
+module.exports = generateDB()
